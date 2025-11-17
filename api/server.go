afc04f4cb72bf0d0236bebc7370fd14a616062e3
Leonid Lugovoi <yotohana.dev@gmail.com>
2025-11-17 14:19:41 +0500

    refactor(grpc): refactor errors in RPC methods

--- api/server.go
+++ api/server.go
@@ -12,10 +12,6 @@ import (
 )
 
 const (
-	ErrUploadFile = "Failed upload file"
-	ErrListFiles = "Failed list files"
-	ErrDownloadFile = "Failed download file"
-	ErrFileNotFound = "File not found"
 	chunkSize = 64 * 1024
 )
 
@@ -26,7 +22,7 @@ type Server struct {
 func (s *Server) List(ctx context.Context, _ *pb.ListRequest) (*pb.ListResponse, error) {
 	items, err := s.storage.GetFileList()
 	if err != nil {
-		return &pb.ListResponse{}, status.Errorf(codes.NotFound, ErrFileNotFound)
+		return nil, status.Errorf(codes.Internal, "failed to read file directory: %v", err)
 	}
 
 	return &pb.ListResponse{Items: items}, nil
@@ -36,37 +32,69 @@ func (s *Server) Upload(stream pb.FileService_UploadServer) error {
 	var id string
 	var file *os.File
 
+	defer func() {
+		if file != nil {
+			file.Close()
+		}
+	}()
+
 	for {
 		req, err := stream.Recv()
 		if err == io.EOF {
 			return stream.SendAndClose(&pb.UploadResponse{Id: id})
 		}
 		if err != nil {
-			return status.Errorf(codes.Internal, ErrUploadFile)
+			if file != nil {
+				os.Remove(file.Name())
+			}
+			return status.Errorf(codes.Internal, "failed to receive data from client: %v", err)
 		}
 
 		if file == nil {
+			if req.GetFilename() == "" {
+				return status.Error(codes.InvalidArgument, "filename cannot be empty")
+			}
+
 			file, id, err = s.storage.CreateFile(req.GetFilename())
+
 			if err != nil {
-				return status.Errorf(codes.Internal, ErrUploadFile)
+				return status.Errorf(codes.Internal, "failed to create file: %v", err)
 			}
 		}
 
-		file.Write(req.GetChunk())
+		_, err = file.Write(req.GetChunk())
+		if err != nil {
+			os.Remove(file.Name())
+			return status.Errorf(codes.Internal, "incomplete write file")
+		}
 	}
 
 	
 }
 
 func (s *Server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadServer) error {
-	fullPath, originalName, err := s.storage.FindFileByID(req.GetId())
+	fileID := req.GetId()
+
+	if fileID == "" {
+		return status.Error(codes.InvalidArgument, "id cannot be empty")
+	}
+
+	fullPath, originalName, err := s.storage.FindFileByID(fileID)
 	if err != nil {
-		return status.Error(codes.NotFound, ErrFileNotFound)
+		if os.IsNotExist(err) {
+			return status.Errorf(codes.NotFound, "file with id '%s' not found", fileID)
+		}
+
+		return status.Errorf(codes.Internal, "failed to locate file: %v", err)
 	}
 
 	file, err := os.Open(fullPath)
 	if err != nil {
-		return status.Error(codes.Internal, "Failed to open file")
+		if os.IsNotExist(err) {
+			return status.Errorf(codes.NotFound, "file not found: %v", err)
+		}
+
+		return status.Errorf(codes.Internal, "failed to open file: %v", err)
 	}
 	defer file.Close()
 
@@ -76,7 +104,7 @@ func (s *Server) Download(req *pb.DownloadRequest, stream pb.FileService_Downloa
 		},
 	})
 	if err != nil {
-		return status.Error(codes.Internal, "Failed to send stream")
+		return status.Errorf(codes.Internal, "Failed to send file metadata: %v", err)
 	}
 
 	buf := make([]byte, chunkSize)
@@ -88,7 +116,7 @@ func (s *Server) Download(req *pb.DownloadRequest, stream pb.FileService_Downloa
 		}
 
 		if err != nil {
-			return status.Error(codes.Internal, "failed to read file")
+			return status.Errorf(codes.Internal, "failed to read file: %v", err)
 		}
 
 		if n > 0 {
