6a9d23ed183a5294ebad9cb3fbc84ee6950b97f1
Leonid Lugovoi <yotohana.dev@gmail.com>
2025-11-18 11:11:52 +0500

    feat(grpc): realize graceful shitdown

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/YotoHana/tages-test-case/internal/api"
 	pb "github.com/YotoHana/tages-test-case/api/proto"
+	"github.com/YotoHana/tages-test-case/internal/api"
 	"github.com/YotoHana/tages-test-case/internal/semaphore"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
+const (
+	listenAddr = ":50051"
+)
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -31,7 +38,28 @@ func main() {
 	}
 	pb.RegisterFileServiceServer(s, fileServer)
 	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		fmt.Printf("gRPC Server is running on port %s\n", listenAddr)
+		fmt.Println("Upload directory: ./uploads")
+		fmt.Println()
+		fmt.Println("Press Ctrl+C to stop...")
+		
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	<-sigChan
+	
+	fmt.Println()
+	fmt.Println("Received shutdown signal...")
+	fmt.Println("Waiting for active requests to complete...")
+
+	s.GracefulStop()
+	
+	fmt.Println("Server stopped gracefully")
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,16 @@ module github.com/YotoHana/tages-test-case
 
 go 1.25.3
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/time v0.14.0
+	google.golang.org/grpc v1.76.0
+	google.golang.org/protobuf v1.36.10
+)
 
 require (
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
-	golang.org/x/time v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251111163417-95abcf5c77ba // indirect
-	google.golang.org/grpc v1.76.0 // indirect
-	google.golang.org/protobuf v1.36.10 // indirect
 )
